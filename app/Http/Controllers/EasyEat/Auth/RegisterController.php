772005cc4e43ef2b548ab00c6e23ede330cb33fb
Rulx Philome Alexis <rulxphilome.alexis@gmail.com>
2019-10-09 14:44:36 -0400

    Credentials creation

---
+++ app/Http/Controllers/EasyEat/Auth/RegisterController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\EasyEat\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use Validator;
+
+class RegisterController extends Controller
+{
+    //
+    public function registerClient(Request $request){
+
+        $validator = Validator::make($request->all(),array(
+            'phoneNumber' => 'required|numeric|unique:users,phoneNumber,loginType',
+            'password' => 'required|regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/',
+            'c_password' => 'required|same:password'
+        ));
+        if($validator->fails()){
+            $response = array(
+                "success" => false,
+                "data"=> "Validation Error",
+                "message" => $validator->errors()
+            );
+
+            return response()->json($response,400);
+        }
+
+        $input = $request->all();
+        $input['name']="";
+        $input['email']="";
+        $input['loginType']= "0"; //Is 0 for Client
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] = $user->createToken('EasyEat')->accessToken;
+        $success['phoneNumber'] = $user->phoneNumber;
+
+        $response = [
+            'success' => true,
+            'data' => $success,
+            'message' => 'User register successfully.'
+        ];
+        return response()->json($response,200);
+    }
+
+    public function registerServiceProvider(Request $request){
+
+        $validator = Validator::make($request->all(),array(
+            'phoneNumber' => 'required|numeric|unique:users,phoneNumber,loginType',
+            'password' => 'required|regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/',
+            'c_password' => 'required|same:password'
+        ));
+        if($validator->fails()){
+            $response = array(
+                "success" => false,
+                "data"=> "Validation Error",
+                "message" => $validator->errors()
+            );
+
+            return response()->json($response,400);
+        }
+
+        $input = $request->all();
+        $input['name']="";
+        $input['email']="";
+        $input['loginType']= "1"; //Is 1 for ServiceProviderProfile
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] = $user->createToken('EasyEat')->accessToken;
+        $success['phoneNumber'] = $user->phoneNumber;
+
+        $response = [
+            'success' => true,
+            'data' => $success,
+            'message' => 'User register successfully.'
+        ];
+        return response()->json($response,200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::post("register/client","EasyEat\Auth\RegisterController@registerClient");
+Route::post("register/serviceprovider","EasyEat\Auth\RegisterController@registerServiceProvider");
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
